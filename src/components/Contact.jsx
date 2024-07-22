8e5a141b901cc7d5f06b832ef0f99eb5786fe80d
Puroo Kulshrestha <purookulshrestha012@gmail.com>
2024-07-22 16:32:59 +0530

    Update Contact.jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -5,22 +5,22 @@ import { AiOutlinePhone, AiOutlineMail } from "react-icons/ai";
 const Contact = () => {
   return (
     <div className="border-b border-neutral-900 pb-20 px-4 md:px-8">
-      <h2 className="my-10 text-center text-3xl md:text-4xl font-bold">
+      <h2 className="my-5 text-center text-2xl md:text-4xl font-bold">
         Get in Touch
       </h2>
       <div className="text-center tracking-tight">
-        <div className="my-5 flex flex-col items-center gap-4 md:gap-6">
-          <p className="font-semibold text-lg md:text-xl flex items-center justify-center gap-2">
+        <div className="my-1 flex flex-col items-center gap-1 md:gap-1">
+          <p className="font-semibold text-lg md:text-xl flex items-center justify-center gap-1">
             <ImLocation className="text-xl md:text-2xl" />
             <span>{CONTACT.address}</span>
           </p>
-          <p className="font-semibold text-lg md:text-xl flex items-center justify-center gap-2">
+          <p className="font-semibold text-lg md:text-xl flex items-center justify-center gap-1">
             <AiOutlinePhone className="text-xl md:text-2xl text-green-500" />
             <span className="text-green-500">{CONTACT.phoneNo}</span>
           </p>
           <a
             href={`mailto:${CONTACT.email}`}
-            className="my-4 font-semibold text-lg md:text-xl flex items-center justify-center gap-2"
+            className="my-1 font-semibold text-lg md:text-xl flex items-center justify-center gap-1"
             style={{ textDecoration: 'none' }}
           >
             <AiOutlineMail className="text-xl md:text-2xl text-red-600" />
