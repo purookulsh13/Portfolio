6b40f67537357572976c9e4bbb31632b3918e8e2
Puroo Kulshrestha <purookulshrestha012@gmail.com>
2024-11-01 10:59:26 +0530

    Update index.js

--- src/constants/index.js
+++ src/constants/index.js
@@ -24,6 +24,13 @@ export const EXPERIENCES = [
       description: `Responsible for leading and managing the club’s technical initiatives. Oversaw the development and implementation of technical solutions to support the club’s projects and organized events.`,
       technologies: ["HTML", "CSS", "JavaScript", "PHP", "MySql"],
     },
+    {
+      year: "August 2024 - Present",
+      role: "A4 Analyst",
+      company: "Capgemini",
+      description: `Recently completed my training program, where I gained hands-on experience in Java backend development with Spring Boot, focusing on microservices architecture. Learned to work with tools and technologies like MySQL, Swagger, and Postman to build, test, and document efficient backend solutions. Now excited to apply these skills and contribute to impactful projects within the team.`,
+      technologies: ["Java", "Spring Boot", "Microservices", "MySQL", "Swagger", "Postman"],
+    },
 ];
 
 export const PROJECTS = [
